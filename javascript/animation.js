250790e14dbe9f6319d0bf834e1cfa8176367755
Gia Alexis Joy <agarado@ucsc.edu>
2020-03-05 15:50:30 -0800

    found a much better way to scroll links into view. i'm a clown.

--- javascript/animation.js
+++ javascript/animation.js
@@ -28,28 +28,8 @@ function fadeNavbarElements() {
     }, 75);
 }
 
-function aboutScroll() {
-    var element = document.getElementById("about");
-    element.scrollIntoView();
-}
-
-function projectScroll() {
-    var element = document.getElementById("projects");
-    element.scrollIntoView();
-}
-
-function resumeScroll() {
-    var element = document.getElementById("resume");
-    element.scrollIntoView();
-}
-
 window.onload = function() {
     // Fade stuff in
     setTimeout(fadeProfileElements, 100);
     setTimeout(fadeNavbarElements, 1000);
-
-    // Scrolling navbar links into view
-    document.getElementById("nav-about").addEventListener("click", aboutScroll);
-    document.getElementById("nav-projects").addEventListener("click", projectScroll);
-    document.getElementById("nav-resume").addEventListener("click", resumeScroll);
 };
