ad0edf247d38724727b65d47126cb0400c6c9443
Gia Alexis Joy <agarado@ucsc.edu>
2020-04-05 23:28:37 -0700

    re-included writing stuff, moved navbar to bottom

---
+++ javascript/navbar.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+//     //change the integers below to match the height of your upper div, which I called
+//     //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+//     //to figure out what the scroll position is when exactly you want to fix the nav
+//     //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+//     //you know the position.
+//     $(window).scroll(function () { 
+  
+//     //   console.log($(window).scrollTop());
+
+//     //   var windowHeight = window.windowHeight();
+  
+//       if ($(window).scrollTop() > 699.2) {
+//         $('#navbar').addClass('navbar-fixed-top');
+//       }
+  
+//       if ($(window).scrollTop() < 700.2) {
+//         $('#navbar').removeClass('navbar-fixed-top');
+//       }
+//     });
+//   });
