aba36936a9fb3fe92410253e2d7a804ce45b85ef
Alexis Garado <alexisgarado@gmail.com>
2023-03-14 14:21:40 -0700

    Update information

--- javascript/themeSwitch.js
+++ javascript/themeSwitch.js
@@ -2,43 +2,43 @@
 // https://inspiredwebdev.com/add-dark-mode-to-your-website/
 
 function toggleTheme() {
-  const themeStylesheet = document.getElementById('theme');
-  const storedTheme = localStorage.getItem('theme');
-  
-  // Check if using mobile display or not
-  const iconTheme = document.getElementById('theme-toggle-icon');
-  var useMobile = window.getComputedStyle(iconTheme).display === 'none';
-
-  const darkIconWide = document.getElementById('moon-wide');
-  const lightIconWide = document.getElementById('sun-wide');
-  const darkIconMobile = document.getElementById('moon-mobile');
-  const lightIconMobile = document.getElementById('sun-mobile');
-
-  if (storedTheme){
-    themeStylesheet.href = storedTheme;
-  }
-  
-  // if it's light -> go dark
-  if (themeStylesheet.href.includes('light')){
-    themeStylesheet.href = 'css/theme/dark.css';
-    if (useMobile) {
-      lightIconMobile.style.display = 'none';
-      darkIconMobile.style.display ='initial';
-    } else {
-      lightIconWide.style.display = 'none';
-      darkIconWide.style.display = 'initial';
-    }
-  } else {
-    // if it's dark -> go light
-    themeStylesheet.href = 'css/theme/light.css';  
-    if (useMobile) {
-      darkIconMobile.style.display = 'none';
-      lightIconMobile.style.display = 'initial';
-    } else {
-      darkIconWide.style.display = 'none';
-      lightIconWide.style.display = 'initial';
-    }
-  }
-  // save the preference to localStorage
-  localStorage.setItem('theme',themeStylesheet.href)
+//   const themeStylesheet = document.getElementById('theme');
+//   const storedTheme = localStorage.getItem('theme');
+//   
+//   // Check if using mobile display or not
+//   const iconTheme = document.getElementById('theme-toggle-icon');
+//   var useMobile = window.getComputedStyle(iconTheme).display === 'none';
+// 
+//   const darkIconWide = document.getElementById('moon-wide');
+//   const lightIconWide = document.getElementById('sun-wide');
+//   const darkIconMobile = document.getElementById('moon-mobile');
+//   const lightIconMobile = document.getElementById('sun-mobile');
+// 
+//   if (storedTheme){
+//     themeStylesheet.href = storedTheme;
+//   }
+//   
+//   // if it's light -> go dark
+//   if (themeStylesheet.href.includes('light')){
+//     themeStylesheet.href = 'css/theme/dark.css';
+//     if (useMobile) {
+//       lightIconMobile.style.display = 'none';
+//       darkIconMobile.style.display ='initial';
+//     } else {
+//       lightIconWide.style.display = 'none';
+//       darkIconWide.style.display = 'initial';
+//     }
+//   } else {
+//     // if it's dark -> go light
+//     themeStylesheet.href = 'css/theme/light.css';  
+//     if (useMobile) {
+//       darkIconMobile.style.display = 'none';
+//       lightIconMobile.style.display = 'initial';
+//     } else {
+//       darkIconWide.style.display = 'none';
+//       lightIconWide.style.display = 'initial';
+//     }
+//   }
+//   // save the preference to localStorage
+//   localStorage.setItem('theme',themeStylesheet.href)
 }
